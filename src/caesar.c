acea7157f48409e1afb5d49d40447a09df5784ec
Nikolaj Krebs <nikolaj2509@gmail.com>
2025-10-19 22:16:20 +0200

    Crypto!

--- src/caesar.c
+++ src/caesar.c
@@ -5,13 +5,21 @@
 
 void caesar_decrypt(char *input){
   for (int i=0; input[i]!='\0';i++){
-    printf("%c",input[i]-3);
+     char c = (input[i]-65 - 3);
+    if (c<0){
+      c=c+26;
+    }
+    printf("%c",c+65);
   }
 }
 
 void caesar_encrypt(char *input){
 
   for (int i=0; input[i]!='\0';i++){
-    printf("%c",input[i]+3);
+   char c = (input[i]-65 +3);
+    if(c>25){
+      c=c-26;
+    }
+    printf("%c",c+65);
   }
 }
